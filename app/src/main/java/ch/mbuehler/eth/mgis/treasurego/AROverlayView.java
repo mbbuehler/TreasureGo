b4ff26d64d75164825232e95e817d18b724cf283
Marcello <marcel.buehler@uzh.ch>
2018-04-25 15:26:18 +0200

    fixed delay in updating imgView

--- app/src/main/java/ch/mbuehler/eth/mgis/treasurego/AROverlayView.java
+++ app/src/main/java/ch/mbuehler/eth/mgis/treasurego/AROverlayView.java
@@ -2,24 +2,16 @@ package ch.mbuehler.eth.mgis.treasurego;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Typeface;
 import android.location.Location;
-import android.opengl.Matrix;
-import android.util.Log;
-import android.view.MotionEvent;
+import android.os.Handler;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.Set;
 
 /**
- * Created by vanagnos on 03.04.2017. Updated by goebelf on 18.04.2018.
+ * Created by vanagnos on 03.04.2017. Updated by goebelf on 18.04.2018 and Marcel on 25.4.18.
  * We extend the View Class of Android (see also: https://developer.android.com/reference/android/view/View.html)
  * A View occupies a rectangular area on the screen and is responsible for drawing and event handling.
  * Here we override the onDraw function and we also draw the ARPoints we want. See the onDraw()
@@ -46,12 +38,6 @@ public class AROverlayView extends View {
      */
     private ARViewUpdater viewUpdater;
 
-    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-
-    LinearLayout lL;
-
-
 
     /**
      * Constructor of the AROverlyView class. Takes the context and List of ARPoints as arguments.
@@ -72,10 +58,6 @@ public class AROverlayView extends View {
 
         // Initialize arActivityView
         viewUpdater.updateARGemsNotFound(arGems.size());
-
-
-
-
 
     }
 
@@ -100,7 +82,7 @@ public class AROverlayView extends View {
     public void updateCurrentLocation(Location currentLocation) {
         this.currentLocation = currentLocation;
 
-        // Here we force the View to be redrawn. So each time we update the projection matrix
+        // Here we force the View to be redrawn. So each time we update the Location
         // the arActivityView is redrawn
         this.invalidate();
     }
@@ -122,58 +104,13 @@ public class AROverlayView extends View {
             return;
         }
 
-        // variables for the point representation
-        final int radius = 30;
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.WHITE);
-        paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
-        paint.setTextSize(60);
-
-        // Transform the ARPoints coordinates from WGS84 to camera coordinates
-        for (ARGem arGem : arGems) {
-
-            // First we transform from GPS coordinates to ECEF coordinates and then to Navigation Coordinates
-            float[] currentLocationInECEF = CoordinateTransformator.WSG84toECEF(currentLocation);
-            float[] pointInECEF = CoordinateTransformator.WSG84toECEF(arGem.getLocation());
-            float[] pointInENU = CoordinateTransformator.ECEFtoENU(currentLocation, currentLocationInECEF, pointInECEF);
-
-            // Afterwards we transform the Navigation coordinates (ENU) to Camera coordinates
-            float[] cameraCoordinateVector = new float[4];
-
-            // To convert ENU coordinate to Camera coordinate, we will multiply camera projection matrix
-            // with ENU coordinate vector, the result is a vector [v0, v1, v2, v3].
-            Matrix.multiplyMV(cameraCoordinateVector, 0, rotatedProjectionMatrix, 0, pointInENU, 0);
-
-
-            // cameraCoordinateVector[2] is z, that always less than 0 to display on right position
-            // if z > 0, the point will display on the opposite
-            if (cameraCoordinateVector[2] < 0) {
-
-                //Then x = (0.5 + v0 / v3) * widthOfCameraView and y = (0.5 - v1 / v3) * heightOfCameraView.
-                float x = (0.5f + cameraCoordinateVector[0] / cameraCoordinateVector[3]) * canvas.getWidth();
-                float y = (0.5f - cameraCoordinateVector[1] / cameraCoordinateVector[3]) * canvas.getHeight();
-
-                // We need to keep track of the position of the ARGems
-                arGem.setX(x);
-                arGem.setY(y);
-
-//                imgView.setVisibility(View.VISIBLE);
-                //arActivityView.addView(imgView);
-
-                // placing the edit text at specific co-ordinates:
-                //canvas.translate(0, 0);
-
-                              canvas.drawCircle(x, y, radius, paint);
-                canvas.drawText(arGem.getName(), x - (30 * arGem.getName().length() / 2), y - 80, paint);
-
-
-                viewUpdater.updateGemParams((int)x, (int)y);
-            }
-        }
+        // Update dynamic elements of View
+        viewUpdater.updateOnDraw(canvas, currentLocation, arGems, rotatedProjectionMatrix);
     }
 
     /**
      * Returns the OnTouchListener for this View.
+     *
      * @param targetTreasureUUID UUID of target Treasure
      * @return instance of AROverlayViewOnTouchListener, initialized with the variables of this View.
      */
--- app/src/main/java/ch/mbuehler/eth/mgis/treasurego/ARViewUpdater.java
+++ app/src/main/java/ch/mbuehler/eth/mgis/treasurego/ARViewUpdater.java
@@ -1,9 +1,16 @@
 package ch.mbuehler.eth.mgis.treasurego;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.location.Location;
+import android.opengl.Matrix;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.Set;
 
 /**
  * Created by marcello on 23/04/18.
@@ -25,7 +32,12 @@ public class ARViewUpdater extends ViewUpdater {
 
     private TextView arGemsNotFoundTextView;
 
+    long lastDrawUpdate = System.currentTimeMillis();
+    long DELTA_DRAW_UPDATE = 1;
 
+
+
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     RelativeLayout imgView;
     RelativeLayout arActivityView;
@@ -90,6 +102,60 @@ public class ARViewUpdater extends ViewUpdater {
     void updateGemParams(int leftMargin, int topMargin){
         params.leftMargin = leftMargin;
         params.topMargin = topMargin;
+    }
+
+    void updateOnDraw(Canvas canvas, Location currentLocation, Set<ARGem> arGems, float[] rotatedProjectionMatrix){
+        if(System.currentTimeMillis() - lastDrawUpdate > DELTA_DRAW_UPDATE) {
+
+            // variables for the point representation
+            final int radius = 30;
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.WHITE);
+            paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+            paint.setTextSize(60);
+
+            int i = 0;
+            // Transform the ARPoints coordinates from WGS84 to camera coordinates
+            for (ARGem arGem : arGems) {
+
+                // First we transform from GPS coordinates to ECEF coordinates and then to Navigation Coordinates
+                float[] currentLocationInECEF = CoordinateTransformator.WSG84toECEF(currentLocation);
+                float[] pointInECEF = CoordinateTransformator.WSG84toECEF(arGem.getLocation());
+                float[] pointInENU = CoordinateTransformator.ECEFtoENU(currentLocation, currentLocationInECEF, pointInECEF);
+
+                // Afterwards we transform the Navigation coordinates (ENU) to Camera coordinates
+                float[] cameraCoordinateVector = new float[4];
+
+                // To convert ENU coordinate to Camera coordinate, we will multiply camera projection matrix
+                // with ENU coordinate vector, the result is a vector [v0, v1, v2, v3].
+                Matrix.multiplyMV(cameraCoordinateVector, 0, rotatedProjectionMatrix, 0, pointInENU, 0);
+
+
+                // cameraCoordinateVector[2] is z, that always less than 0 to display on right position
+                // if z > 0, the point will display on the opposite
+                if (cameraCoordinateVector[2] < 0) {
+
+                    //Then x = (0.5 + v0 / v3) * widthOfCameraView and y = (0.5 - v1 / v3) * heightOfCameraView.
+                    float x = (0.5f + cameraCoordinateVector[0] / cameraCoordinateVector[3]) * canvas.getWidth();
+                    float y = (0.5f - cameraCoordinateVector[1] / cameraCoordinateVector[3]) * canvas.getHeight();
+
+                    // We need to keep track of the position of the ARGems
+                    arGem.setX(x);
+                    arGem.setY(y);
+
+                    canvas.drawCircle(x, y, radius, paint);
+                    canvas.drawText(arGem.getName(), x - (30 * arGem.getName().length() / 2), y - 80, paint);
+
+                    i += 1 % 5;
+                    if(i==1)
+                        this.updateGemParams((int) x, (int) y);
+                }
+            }
+            lastDrawUpdate = System.currentTimeMillis();
+
+            // Make sure the ARGems position gets updated.
+            imgView.requestLayout();
+        }
     }
 }
 
